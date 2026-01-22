ceb9dd774b7629d53d41c715ca3587c773fc24b8
Mozilan1216 <1564448710@qq.com>
2026-01-22 10:41:00 +0800

    增加重定位所需代码，便于进行后续重定位的操作

--- src/camera/launch/d435i_launch.py
+++ src/camera/launch/d435i_launch.py
@@ -27,7 +27,12 @@ def generate_launch_description():
                 'unite_imu_method': '2',        # 强制合并 IMU 话题
                 'enable_sync': 'true',          # 强制开启 RGB/Depth 同步
                 'camera_name': 'camera',        # 确保节点名与 YAML 匹配
-                'namespace': 'camera'           # 确保命名空间一致
+                'namespace': 'camera',          # 确保命名空间一致
+                # 重定位
+                'align_depth.enable': 'true',  # 必须，确保话题为 aligned_depth_to_color
+                'enable_sync': 'true',         # 必须，确保 RGB 和 Depth 时间戳同步
+                'global_time_enabled': 'true',  # 强制使用系统时间戳
+                'initial_reset': 'true',        # 启动时重置相机，清除硬件缓存
             }.items()
         )
     ])
